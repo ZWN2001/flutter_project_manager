7cb9751d408da9a97c453e4f4ed1e29e3bb3421e
赵炜宁 <2134356214@qq.com>
2021-12-19 22:43:21 +0800

    归档界面

---
+++ lib/view/archived_demand_board/archived_demand_board_logic.dart
@@ -0,0 +1,31 @@
+import 'package:admin/controllers/api.dart';
+import 'package:get/get.dart';
+
+class ArchivedDemandBoardLogic extends GetxController {
+  RxList<Map> allDoneDemands = [{}].obs;
+  // List allDemandsTimeLine = [];
+  String demandFileUrl = '';
+
+  @override
+  void onInit() {
+    super.onInit();
+    getAllDoneDemandList();
+
+  }
+
+  void getAllDoneDemandList()  {
+    DemandAPI().getDoneDemandnList().then((value){
+      allDoneDemands.value = value;
+      print(value);
+      update();
+    });
+  }
+
+}
+
+class ArchivedDemandBoardBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut(() => ArchivedDemandBoardLogic());
+  }
+}
